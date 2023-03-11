ee1e23d4112e5ccdc842f50aa58b7dbdc22284fa
Nithik Balachandran <nithikbala@gmail.com>
2023-03-11 00:17:18 -0500

    card deck

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/nithikb/src/Poker-GTO/cards
+# Build directory: /Users/nithikb/src/Poker-GTO/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
